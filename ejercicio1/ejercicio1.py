f091ed1d95c962afc1c440fe87e77109eb119022
AndresLostaunau <39714785+AndresLostaunau@users.noreply.github.com>
2021-06-07 20:00:01 -0500

    eje1

--- ejercicio1/ejercicio1.py
+++ ejercicio1/ejercicio1.py
@@ -1,38 +1,43 @@
 import nltk
-from nltk.corpus import stopwords
-nltk.download("punkt")
-nltk.download('stopwords')
-
-texto = "Hola mi amor. Aqui estoy en los causas de UTEC aprendiendo indice invertido"
-palabras = nltk.word_tokenize(texto.lower())
-print(palabras)
-
-
-with open('stoplist.txt') as file:
-    stoplist = [line.lower().strip() for line in file]
-stoplist += ['.','?','-','hola']
-
-palabras_limpias = palabras[:]
-for token in palabras:
-    if token in stoplist:
-        palabras_limpias.remove(token)
-print(palabras)
-print(palabras_limpias)
-
-
-from nltk.stem import PorterStemmer
-stemmer = PorterStemmer()
-words = ["program", "programs", "programer", "programing", "programers"]
-for w in words:
-    print(w, " =>", stemmer.stem(w))
-
-######
 from nltk.stem.snowball import SnowballStemmer
-stemmer = SnowballStemmer('spanish')
-words = ["amor", "amaras", "amores"]
-for w in words:
-    print(w, " =>", stemmer.stem(w))
+# nltk.download("punkt")
+# nltk.download('stopwords')
 
-stemmer = SnowballStemmer('spanish')
-for w in palabras_limpias:
-    print(w, " =>", stemmer.stem(w))
+listaRecurrente = {}
+
+with open('stoplist.txt') as filterFile:
+    stoplist = [line.lower().strip() for line in filterFile]
+stoplist += ['.', '?', '-', ';', ':', ',', '\'', '\"', '!', '¿', '¡', '»', '(', ')', '«']
+
+
+def readFile(name):
+    f = open(name, "r", encoding='utf8')
+    stemmer = SnowballStemmer('spanish')
+    texto = f.read()
+    palabras = nltk.word_tokenize(texto.lower())
+    for token in palabras:
+        word = stemmer.stem(token)
+        if word not in stoplist:
+            if listaRecurrente.get(word) is not None:
+                listaRecurrente[word] = listaRecurrente.get(word) + 1
+            else:
+                listaRecurrente[word] = 1
+
+
+readFile("libro1.txt")
+readFile("libro2.txt")
+readFile("libro3.txt")
+readFile("libro4.txt")
+readFile("libro5.txt")
+readFile("libro6.txt")
+
+listaRecurrente = sorted(listaRecurrente.items(), key=lambda x: x[1], reverse=True)
+
+print(listaRecurrente)
+
+file = open("freqWord.txt", "w", encoding='utf8')
+for i in range(500):
+    if listaRecurrente[i] is None:
+        break
+    else:
+        file.write(listaRecurrente[i][0]+'\n')
